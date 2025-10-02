7c158cfabb1c8cfbdae0a544dd1f8719cbc15536
chioca <kraistine21@gmail.com>
2025-10-02 08:22:18 -0700

    feat: refactor ZSet functions to improve score updating and node lookup

--- z_set.cpp
+++ z_set.cpp
@@ -2,11 +2,17 @@
 
 #include <cstring>
 
+#include "common.h"
+struct HKey {
+  Hnode node;
+  const char *name = NULL;
+  size_t len = 0;
+};
 ZNode *znode_new(const char *name, size_t len, double score) {
   ZNode *node = (ZNode *)malloc(sizeof(ZNode) + len);
   avl_init(&node->tree);
   node->hmap.next = nullptr;
-  node->hmap.hcode = (uint64_t)std::hash<std::string>()(name);
+  node->hmap.hcode = str_hash((uint8_t *)name, len);
   node->score = score;
   node->len = len;
   memcpy(&node->name[0], name, len);
@@ -43,4 +49,42 @@ void tree_insert(ZSet *zset, ZNode *node) {
   *from = &node->tree;
   node->tree.parent = parent;
   zset->root = avl_fix(&node->tree);
+}
+
+// 更新已有节点的score
+static void zset_update(ZSet *zset, ZNode *node, double score) {
+  if (node->score == score) {
+    return;
+  }
+
+  zset->root = avl_del(&node->tree);
+  avl_init(&node->tree);
+  node->score = score;
+  tree_insert(zset, node);
+}
+
+ZNode *zset_lookup(ZSet *zset, const char *name, size_t len) {
+  if (!zset->root) {
+    return nullptr;
+  }
+
+  HKey key;
+  key.node.hcode = str_hash((uint8_t *)name, len);
+  key.name = name;
+  key.len = len;
+  Hnode *found = hm_lookup(&zset->hmap, &key.node, entry_eq);
+  return found ? container_of(found, ZNode, hmap) : nullptr;
+}
+
+bool zset_insert(ZSet *zset, const char *name, size_t len, double socre) {
+  ZNode *node = zset_lookup(zset, name, len);
+  if (node) {
+    zset_update(zset, node, socre);
+    return false;
+  } else {
+    node = znode_new(name, len, socre);
+    hm_insert(&zset->hmap, &node->hmap);
+    tree_insert(zset, node);
+    return true;
+  }
 }
